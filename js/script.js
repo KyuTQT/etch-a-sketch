75d294dcdf57cd0bb0743ce7bd755f8dada564af
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-14 10:49:52 +0800

    Add functionality to normal and party paint

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ let clearPaint = document.querySelector('#clear');
 
 let isMousePressed = false;
 let gridCanvas;
+let isPartyOn = false;
+let randomizedRGBColor = [];
 
 function fillUpContainer(gridSize){
     gridCanvas = gridSize;
@@ -37,6 +39,14 @@ clearPaint.addEventListener('click', function(){
     fillUpContainer(gridCanvas);
 })
 
+normalPaint.addEventListener('click', function(){
+    isPartyOn = false;
+})
+
+partyPaint.addEventListener('click', function(){
+    isPartyOn = true;
+})
+
 divContainer.addEventListener('mousedown', function(){
     isMousePressed = true;
 })
@@ -45,14 +55,27 @@ divContainer.addEventListener('mouseup', function(){
     isMousePressed = false;
 })
 
+function gridColor(selectedGrid){
+    if(!isPartyOn){
+        selectedGrid.style.backgroundColor = 'black';
+    }
+    else{
+        for(let i = 0; i <= 2; i++){
+            let randomNumber = Math.floor(Math.random() * 256);
+            randomizedRGBColor[i] = randomNumber;
+        }
+        selectedGrid.style.backgroundColor = `rgb(${randomizedRGBColor[0]}, ${randomizedRGBColor[1]}, ${randomizedRGBColor[2]})`;
+    }
+}
+
 divContainer.addEventListener('mousemove', function(e){
     if(isMousePressed){
-        e.target.style.backgroundColor = 'black';
+        gridColor(e.target);
     }
 })
 
 divContainer.addEventListener('click', function(e){
-    e.target.style.backgroundColor = 'black';
+    gridColor(e.target);
 })
 
 
